104522cc1c6940fc96baa08ac542277a42620d4b
s-marsi <soufianemarsi1999@gmail.com>
2024-12-28 14:18:15 +0100

    Initial commit

---
+++ src/test/java/com/openclassrooms/edoctorat/EdoctoratApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openclassrooms.edoctorat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdoctoratApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
